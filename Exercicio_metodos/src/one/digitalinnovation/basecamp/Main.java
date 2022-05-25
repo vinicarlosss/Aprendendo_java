e021edd189191b4c0dedfb47152244630fde68de
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2022-05-24 21:59:13 -0300

    exercicio de metodos

---
+++ Exercicio_metodos/src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exercício da calculadora");
+        Calculadora.soma(3,6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7,8);
+        Calculadora.divisao(2,1);
+    }
+}
