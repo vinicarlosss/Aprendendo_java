f4ca694be29641b608826c86206a4e4970ea062d
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2022-05-25 16:08:03 -0300

    sobrecarga de métodos

---
+++ Sobrecarga/src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+    public static void main(String[] args) {
+        Quadrilatero.area(2);
+        Quadrilatero.area(2,3);
+        Quadrilatero.area(3,2,8);
+    }
+}
